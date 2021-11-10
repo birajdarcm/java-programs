4ee462f3866d15f156ba28a90abd8ed367537488
birajdarcm <birajdarcm@gmail.com>
2021-11-10 19:20:54 +0530

    v1

---
+++ src/basicJavaPrograms/readingAndPrintingDataFromUser.java
@@ -0,0 +1,12 @@
+package basicJavaPrograms;
+
+import java.util.Scanner;
+
+public class readingAndPrintingDataFromUser {
+	
+	public static void main(String [] args) {
+	
+	System.out.println("Hello World");
+
+}
+}
